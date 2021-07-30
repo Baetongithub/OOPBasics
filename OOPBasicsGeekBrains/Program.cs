021d543730d94a0ccdb1bc00f0fdff12a10e7d0d
Baet <toktonbekovbaet@gmail.com>
2021-07-30 11:58:32 +0600

    Добавьте файлы проекта.

---
+++ OOPBasicsGeekBrains/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace OOPBasicsGeekBrains
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            System.Windows.Forms.Application.EnableVisualStyles();
+            System.Windows.Forms.Application.SetCompatibleTextRenderingDefault(false);
+            System.Windows.Forms.Application.Run(new Form1());
+
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
